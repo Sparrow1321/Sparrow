cc03a3c5f6c758b84cfb99d4d6153dd60abe51b3
payal neware <149008768+Sparrow1321@users.noreply.github.com>
2023-10-29 00:28:49 +0530

    Initial commit

---
+++ reactjs/src/data.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		id: 1,
+		name: 'Vijay Devarakonda',
+		age: 28,
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgd7ee2vROA8C7MQOf58uPDYiVnGL3j-c1nQ&usqp=CAU'
+	},
+	{
+		id: 2,
+		name: 'Rashmika Mandanna',
+		age: 32,
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWcfgrdg44GRye91-n6EKUmSevvmIlU5x1wg1A3h3fjmUdmv6QC5dkZcyAwkT5yUcw9Yg&usqp=CAU'
+	},
+	{
+		id: 3,
+		name: 'Ritesh Deshmukh',
+		age: 36,
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNO8AmOM6Hdh23xHUQvuNfpV1p5iwfa9ZJpg&usqp=CAU'
+	},
+	{
+		id: 4,
+		name: 'Genelia Deshmukh',
+		age: 34,
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSPjetWqAevOxpMYoe0aixikAtKh4YKZgYlg&usqp=CAU'
+	},
+	{
+		id: 5,
+		name: 'Madhuri Dixit',
+		age: 29,
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNVqY8iT9He1-meWCSwKmy4IEGRB9FmbkNSQ&usqp=CAU'
+	}
+]
